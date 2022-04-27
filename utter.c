3f7deb278d7dc647812b14bf5bb2dabda8cb258a
CasuallyAlive <jordy.larrearodriguez@gmail.com>
2022-04-27 15:03:02 -0600

    Passing all simple tests, aside from a small bug in sync. TODO- fix bug and get concurrency working on my implementation.

--- utter.c
+++ utter.c
@@ -19,18 +19,19 @@ static void clienterror(int fd, char *cause, char *errnum,
                         char *shortmsg, char *longmsg);
 static void print_stringdictionary(dictionary_t *d);
 
-static void serve_request(int fd, char *);
+static void serve_request(int,dictionary_t *, char *);
 
 static void do_listen(int, dictionary_t*);
 static void do_utter(int, dictionary_t*);
 static void do_shh(int, dictionary_t*);
 static void do_sync(int, dictionary_t*);
-static void makeUserID(char* user, char* id);
+static char *makeUserID(char* user);
+static char* getUserUtters(int, dictionary_t *, const char **);
 
 struct dictionary_t *users_posts_dict;
-char *HOST_NAME, *PORT;
+char *HOST_NAME = NULL, *PORT = NULL;
 int id_num = 0;
-const size_t MAX_ID = 150;
+const size_t MAX_ID = 50;
 
 #ifdef SILENCE_PRINTF
 #define printf(...)
@@ -43,9 +44,6 @@ int main(int argc, char **argv) {
 
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
-  
-  HOST_NAME = hostname;
-  PORT = port;
 
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
@@ -57,6 +55,8 @@ int main(int argc, char **argv) {
   }
 
   listenfd = Open_listenfd(argv[1]);
+
+  PORT = strdup(argv[1]);
 
   /* Don't kill the server if there's an error, because
      we want to survive errors due to a client. But we
@@ -73,6 +73,9 @@ int main(int argc, char **argv) {
       Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
           port, MAXLINE, 0);
       printf("Accepted connection from (%s, %s)\n", hostname, port);
+      if(HOST_NAME == NULL){
+        HOST_NAME = strdup(hostname);
+      }
 
       doit(connfd);
       Close(connfd);
@@ -162,7 +165,46 @@ void doit(int fd){
  * @param query 
  */
 static void do_listen(int fd, dictionary_t* query){
+  char *user = dictionary_get(query, "user");
 
+  if(dictionary_count(query) < 1 || user == NULL){
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  dictionary_t *user_messages_dict;
+
+  if((user_messages_dict = dictionary_get(users_posts_dict, user)) == NULL){
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+
+  size_t count = dictionary_count(user_messages_dict);
+  if(count == 0){
+    serve_request(fd, query,"");
+    return;
+  }
+  // print_stringdictionary(user_messages_dict);
+
+  const char **utter_ids = dictionary_keys(user_messages_dict);
+ 
+  char *body = getUserUtters(count, user_messages_dict, utter_ids);
+  serve_request(fd, query, body);
+}
+
+static char* getUserUtters(int count, dictionary_t *user_messages_dict, const char **utter_ids){
+  char *posts[count + 1];
+  posts[count] = NULL;
+  char *message;
+
+  for(size_t i = 0; i < count; i++){
+    message = dictionary_get(user_messages_dict, utter_ids[i]);
+    posts[i] = append_strings(utter_ids[i], " ", message, NULL);
+  }
+  return join_strings((const char * const*) posts, '\n');
 }
 /**
  * @brief POST /utter?user=‹user›&utterance=‹the message› — This makes a new utter post for user.  
@@ -177,26 +219,28 @@ static void do_utter(int fd, dictionary_t* query){
   char *user = dictionary_get(query, "user");
   char *message = dictionary_get(query, "utterance");
 
-  if(dictionary_count(query) < 1 && (user == NULL || message == NULL)){
+  if(dictionary_count(query) < 1 || user == NULL || message == NULL){
     //report error
-      clienterror(fd, "POST","400", "Bad Request",
-              "Utter did not recognize the request");
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
   }
 
-  char id[MAX_ID];
-  makeUserID(user, id);
+  char *id = makeUserID(user);
   dictionary_t *user_utter_posts;
 
   if(dictionary_get(users_posts_dict, user) == NULL){
     user_utter_posts = make_dictionary(COMPARE_CASE_SENS, NULL);
 
-    dictionary_set(user_utter_posts, id, message); //Set of messages
-    dictionary_set(users_posts_dict, user, user_utter_posts);
-  }else{
+    dictionary_set(user_utter_posts, id, strdup(message)); //Set of messages
+    dictionary_set(users_posts_dict, strdup(user), user_utter_posts);
+  }else{//
     user_utter_posts = dictionary_get(users_posts_dict, user);
-    dictionary_set(user_utter_posts, id, message);
+    dictionary_set(user_utter_posts, id, strdup(message));
   }
-  serve_request(fd,id);
+  // print_stringdictionary(user_utter_posts);
+  serve_request(fd, query, id);
 }
 /**
  * @brief POST /shh?user=‹user›&id=‹messageID› — Remove the post with the given ID from the user's posts. 
@@ -206,7 +250,31 @@ static void do_utter(int fd, dictionary_t* query){
  * @param query 
  */
 static void do_shh(int fd, dictionary_t* query){
+  char *user = strdup(dictionary_get(query, "user"));
+  char *message_id = strdup(dictionary_get(query, "id"));
 
+  if(dictionary_count(query) < 1 || user == NULL){
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  dictionary_t *user_messages_dict;
+
+  if((user_messages_dict = dictionary_get(users_posts_dict, user)) == NULL){
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  char *message;
+  if((message = dictionary_get(user_messages_dict, message_id)) == NULL){
+    serve_request(fd, query, "No such id.");
+    return;
+  }
+
+  dictionary_remove(user_messages_dict, message_id);
+  serve_request(fd, query, append_strings("Successfully removed id: ", message_id, NULL));
 }
 /**
  * @brief POST /sync?user=‹user›&hostname=‹hostname›&port=‹port› — Contacts an utter server running on ‹host› at ‹port› to get all of the posts of ‹user›, 
@@ -217,20 +285,97 @@ static void do_shh(int fd, dictionary_t* query){
  * @param query 
  */
 static void do_sync(int fd, dictionary_t* query){
+  char *user = strdup(dictionary_get(query, "user"));
+  char *host_name = strdup(dictionary_get(query, "hostname"));
+  char *port = strdup(dictionary_get(query, "port"));
 
+  if(dictionary_count(query) < 1 || user == NULL || host_name == NULL || port == NULL){
+    clienterror(fd, "POST","400", "Bad Request",
+            "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  } 
+
+  char buf[MAXBUF], response[MAXBUF];
+  int con = open_clientfd(host_name, port);
+
+  sprintf(buf, "GET /listen?user=%s HTTP/1.0\r\n\r\n", query_encode(user));
+  Rio_writen(con, buf, strlen(buf));
+  Shutdown(con, SHUT_WR);
+
+  rio_t rio;
+  Rio_readinitb(&rio, con);
+
+  if(Rio_readlineb(&rio, response, MAXLINE) <= 0){
+      clienterror(fd, "POST", "400", "Bad Request", "Utter did not recognize the request");
+      serve_request(fd, query, "error");
+      return;
+  }
+  char *status, *version, *desc;
+  if(!parse_status_line(response, &version, &status, &desc)){
+    clienterror(fd, "POST", "400", "Bad Request", "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  // checks for wrong version or wrong response.
+  if (strcasecmp(version, "HTTP/1.0")
+    && strcasecmp(version, "HTTP/1.1")) {
+    clienterror(fd, version, "501", "Not Implemented",
+      "Utter does not implement that version");
+    serve_request(fd, query, "error");
+    return;
+  }
+  if (strcasecmp(desc, "OK") && strcasecmp(status, "200")) {
+    clienterror(fd, status, "501", "Not Implemented",
+      "Bad response");
+    serve_request(fd, query, "error");
+    return;
+  }
+
+  dictionary_t *headers = read_requesthdrs(&rio);
+  if( headers == NULL || dictionary_count(headers) < 1){
+    clienterror(fd, "POST", "400", "Bad Request", "Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  print_stringdictionary(headers); // we get "Content-length"  and "Content-type"
+
+  char *content_length = dictionary_get(headers, "content-length");
+  int count = atoi(content_length);
+  
+  if(count == 0){
+    clienterror(fd, "POST", "400", "Bad Request","Utter did not recognize the request");
+    serve_request(fd, query, "error");
+    return;
+  }
+  Rio_readnb(&rio, response, count);
+  response[count] = 0; // NULL terminating. 
+  dictionary_t *user_utters = dictionary_get(users_posts_dict, user);
+  if (user_utters == NULL){ // make a new dictionary for a user's post on this server in case it doesn't exist
+    user_utters = make_dictionary(COMPARE_CASE_SENS, NULL);
+    dictionary_set(users_posts_dict, user, user_utters);
+  }
+
+  char **new_utters = split_string(response, '\n');
+  char **utter;
+  for(int i = 0; new_utters[i] != NULL; i++){ // Null terminated char array
+    if(strcmp(new_utters[i], "\r") == 0)
+      continue;
+    utter = split_string(new_utters[i], ' '); // <utter_id> <message>
+    dictionary_set(user_utters, strdup(utter[0]), strdup(utter[1]));
+  }
+  
+  char *body = getUserUtters(dictionary_count(user_utters), user_utters, dictionary_keys(user_utters));
+
+  serve_request(fd, query, body);
+  Close(con);
 }
 
-static void makeUserID(char* user, char* id){
+static char* makeUserID(char* user){
   char num_str[10];
+  
   sprintf(num_str, "%d", id_num++);
-
-  strcat(id, HOST_NAME);
-  strcat(id, "_");
-  strcat(id, PORT);
-  strcat(id, "_");
-  strcat(id, user);
-  strcat(id, "_");
-  strcat(id, num_str);
+  return append_strings(HOST_NAME,"_",PORT,"_",user,"_",num_str,NULL);
 }
 /*
  * read_requesthdrs - read HTTP request headers
@@ -287,11 +432,11 @@ static char *ok_header(size_t len, const char *content_type) {
 /*
  * serve_request - example request handler
  */
-static void serve_request(int fd, char *body) {
+static void serve_request(int fd, dictionary_t *query, char *body) {
   size_t len;
   char *header;
 
-  // body = strdup("hello world!\n");
+  body = strdup(append_strings(body, "\r\n", NULL));
 
   len = strlen(body);
 
@@ -306,7 +451,8 @@ static void serve_request(int fd, char *body) {
   /* Send response body to client */
   Rio_writen(fd, body, len);
 
-  // free(body);
+  free(body);
+  // free(query);
 }
 
 /*
